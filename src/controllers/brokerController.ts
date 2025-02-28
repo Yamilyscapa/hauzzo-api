b04982af417acc07dbf8d3efd852d6dad09fd31c
Yamil Yscapa <yamilyscapa@MacBook-Pro-de-Yamil.local>
2025-02-28 16:12:14 -0600

    Add create brokers function

--- src/controllers/brokerController.ts
+++ src/controllers/brokerController.ts
@@ -1,9 +1,50 @@
 import { pool } from '../database/client';
 import { Broker } from '../types/global';
+import { hashPassword } from '../utils/passwordHash'
 
 interface stdRes {
     broker: Broker | null,
     error: any
+}
+
+export async function createBroker(body: Broker): Promise<stdRes> {
+    let response: stdRes = {
+        broker: null,
+        error: null
+    }
+    const ROLE = 'broker'
+
+    try {
+        const { firstName, lastName, email, phone, password } = body
+        const hashedPassword = await hashPassword(password)
+
+        // Check if the required fields are present 
+        if (!firstName || !lastName || !email || !password) {
+            throw new Error('Missing required fields')
+        }
+
+        // Check if the user already exists
+        const brokerAlreadyExists = await getBrokerByEmail(email)
+
+        if (brokerAlreadyExists.broker) {
+            throw new Error('Broker already exists, email already in use')
+        }
+
+        const query = {
+            text: 'INSERT INTO brokers (first_name, last_name, email, phone, password, role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+            values: [firstName, lastName, email, phone, hashedPassword, ROLE]
+        }
+
+        const { rows } = await pool.query(query)
+
+        response.broker = rows[0]
+        response.error = null
+        return response
+    } catch (error: Error | any) {
+        response.broker = null
+        response.error = error
+        return response
+    }
 }
 
 export async function getBrokerById(id: string): Promise<stdRes> {
@@ -35,7 +76,9 @@ export async function getBrokerById(id: string): Promise<stdRes> {
     }
 }
 
-export async function getBrokerByEmail(email: string): Promise<{ user: Broker | null, error: any }> {
+export async function getBrokerByEmail(email: string): Promise<stdRes> {
+    let response = {} as stdRes
+
     try {
         const query = {
             text: 'SELECT * FROM brokers WHERE email = $1',
@@ -45,14 +88,19 @@ export async function getBrokerByEmail(email: string): Promise<{ user: Broker | 
         const { rows } = await pool.query(query)
 
         // Return an object with the user and error to handle the response in the controller 
-        if (rows.length === 0) return { user: null, error: false }
-
-        return { user: rows[0], error: false }
-    } catch (error) {
-        if (error instanceof Error) {
-            return { user: null, error: error.message }
+        if (rows.length === 0) {
+            response.broker = null
+            response.error = false
         } else {
-            return { user: null, error }
+            response.broker = rows[0]
+            response.error = false
         }
+
+        return response
+    } catch (error: Error | any) {
+        response.broker = null
+        response.error = error
+
+        return response
     }
 }
--- src/services/brokerService.ts
+++ src/services/brokerService.ts
@@ -4,8 +4,37 @@ const router = Router()
 
 // Imports
 import { errorResponse, successResponse } from '../helpers/responseHelper';
-import { getBrokerById } from '../controllers/brokerController';
+import { getBrokerById, getBrokerByEmail, createBroker } from '../controllers/brokerController';
+import { Broker } from '../types/global';
 
+// POST /brokers/new
+router.post('/new', async (req: Request, res: Response) => {
+    try {
+        const body = req.body
+ 
+        if (Object.keys(body).length === 0) {
+            errorResponse(res, 'Empty body', 400)
+            return;
+        }
+
+        const { broker, error } = await createBroker(body)
+
+        if (error) {
+            errorResponse(res, error.message, 400, error)
+            return
+        } else if (!broker) {
+            errorResponse(res, 'Error creating the broker', 400)
+            return
+        } else {
+            successResponse(res, broker as Broker, 'Broker created', 201)
+            return
+        }
+    } catch (error) {
+        errorResponse(res, 'Error creating the broker', 400)
+    }
+})
+
+// GET /brokers/:id
 router.get('/:id', async (req: Request, res: Response) => {
     try {
 
@@ -17,6 +46,31 @@ router.get('/:id', async (req: Request, res: Response) => {
         }
 
         const { broker, error } = await getBrokerById(id);
+
+        if (error) {
+            errorResponse(res, error.message, 400, error)
+        } else if (!broker) {
+            errorResponse(res, 'Broker not found', 404)
+        } else {
+            successResponse(res, broker, 'Broker found', 200)
+        }
+    } catch (error) {
+        errorResponse(res, 'Error finding the broker', 400)
+    }
+})
+
+// GET /brokers/:email
+router.get('/email/:email', async (req: Request, res: Response) => {
+    try {
+
+        const email: string = req.params.email
+
+        if (!email) {
+            errorResponse(res, 'Invalid email', 400)
+            return;
+        }
+
+        const { broker, error } = await getBrokerByEmail(email);
 
         if (error) {
             errorResponse(res, error.message, 400, error)
