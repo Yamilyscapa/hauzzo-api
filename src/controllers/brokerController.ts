4c96ca92c065a2a3c3a95b92140cd5ade821e522
yam <yamil.yscapa@gmail.com>
2025-05-26 16:44:39 -0600

    Generate UUID from createUser function instead of from DB

--- src/controllers/brokerController.ts
+++ src/controllers/brokerController.ts
@@ -3,6 +3,7 @@ import { Broker } from '../types/global'
 import { hashPassword } from '../utils/passwordHash'
 import validateEmail from '../utils/emailValidation.'
 import validatePassword from '../utils/passwordValidation'
+import { v4 } from 'uuid'
 
 // Standarize the response of the service functions
 interface stdRes {
@@ -19,6 +20,18 @@ export async function createBroker(body: Broker): Promise<stdRes> {
 
   try {
     const { firstName, lastName, email, phone, password } = body
+    const id = v4()
+
+    // Check if the required fields are present
+    if (!firstName || !lastName || !email || !password) {
+      throw new Error('Missing required fields')
+    }
+
+
+    if (email && !validateEmail(email)) {
+      throw new Error('Invalid email format')
+    }
+
     //  Validate password
     if (!validatePassword(password)) {
       throw new Error(
@@ -28,15 +41,6 @@ export async function createBroker(body: Broker): Promise<stdRes> {
 
     const hashedPassword = await hashPassword(password)
 
-    if (email && !validateEmail(email)) {
-      throw new Error('Invalid email format')
-    }
-
-    // Check if the required fields are present
-    if (!firstName || !lastName || !email || !password) {
-      throw new Error('Missing required fields')
-    }
-
     // Check if the user already exists
     const brokerAlreadyExists = await getBrokerByEmail(email)
 
@@ -45,8 +49,8 @@ export async function createBroker(body: Broker): Promise<stdRes> {
     }
 
     const query = {
-      text: 'INSERT INTO brokers (first_name, last_name, email, phone, password, role) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
-      values: [firstName, lastName, email, phone, hashedPassword, ROLE],
+      text: 'INSERT INTO brokers (first_name, last_name, email, phone, password, role, id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+      values: [firstName, lastName, email, phone, hashedPassword, ROLE, id],
     }
 
     const { rows } = await pool.query(query)
